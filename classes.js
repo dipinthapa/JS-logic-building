842a475696682e977025cad2354d6ebefc7c42ea
unknown <dipin.thapa1@gmail.com>
2025-12-30 18:20:10 +0545

    JS practice

---
+++ classes.js
@@ -0,0 +1,34 @@
+class User {
+    constructor(username, email, password){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+      
+     encryptPassword(){
+        return `${this.password}abc`
+     }
+
+     changeUsername(){
+        return `${this.username.toUpperCase()}`
+     }
+
+} 
+
+const dipin = new User("dipin", "demo@gmail.com", "qwerty")
+ console.log(dipin.encryptPassword());
+ console.log(dipin.changeUsername());
+
+
+     class Admin extends User{
+        constructor(username, email, password, role){
+            super(username, email, password)
+            this.role = role
+        }
+
+        addMember(){
+            console.log(`A new member was added by ${this.username}`);
+        }
+     }
+
+     
---
+++ practice.js
@@ -0,0 +1,6 @@
+// String Manipulation: 
+// Write a program that takes a string as input and converts it to uppercase.
+// apple -> APPLE
+
+let 
+
