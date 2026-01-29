0af880c2b1178361407e05d1e3bc092a99887f61
unknown <dipin.thapa1@gmail.com>
2026-01-29 21:21:58 +0545

    regExp basic

---
+++ reg-exp/reg.js
@@ -0,0 +1,5 @@
+// Regular expressions: Used to describe and select patterns inside of string data
+
+// [ used to include ] , [^ select except ] ,
+/*( capturing group ), [ character set ], A-Z range, ([A-Z]) this matches characters ranging from A to Z
+*/
