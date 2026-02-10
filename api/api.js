e6f8cff1be58d5559cf2ffca0ad46e510d1f80e3
unknown <dipin.thapa1@gmail.com>
2026-02-10 20:35:30 +0545

    fetch practice

--- api/api.js
+++ api/api.js
@@ -5,7 +5,10 @@ the fetch() method is used to fetch a resource.
  
 Syntax: let promise = fetch(url.[option])        */
 
-const URL = "https://cat-fact.herokapp.com/facts";
+const URL = "https://cat-fact.herokuapp.com/facts";
 
-let promise = fetch(URL);
-console.log(promise)
+const getFacts = async () => {
+    let response = await fetch(URL);
+    console.log(response)
+
+}
