a37c1435d5f942b7914d1f4709d8d05ded0e8b94
unknown <dipin.thapa1@gmail.com>
2026-02-11 18:01:55 +0545

    fatch api trial

--- api/api.js
+++ api/api.js
@@ -5,10 +5,20 @@ the fetch() method is used to fetch a resource.
  
 Syntax: let promise = fetch(url.[option])        */
 
-const URL = "https://cat-fact.herokuapp.com/facts";
+const URL = "https://fakestoreapi.com/products/1";
+const dataP = document.querySelector("#desc");
+const btn = document.querySelector("#btn");
 
-const getFacts = async () => {
+const getSomething = async () => {
     let response = await fetch(URL);
     console.log(response)
-
+    let aData = await response.json();
+    console.log(aData.description);
+    dataP.innerText = aData.description;
 }
+
+// fetch('https://fakestoreapi.com/products/1')
+//             .then(res=>res.json())            
+//             .then(json=>console.log(json))
+
+btn.addEventListener("click", getSomething);
