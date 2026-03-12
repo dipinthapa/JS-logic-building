13a690965af163d0dfac6b833531bbd4c1677c1b
unknown <dipin.thapa1@gmail.com>
2026-03-12 11:17:00 +0545

    logic practice

--- api/api.js
+++ api/api.js
@@ -5,6 +5,8 @@ the fetch() method is used to fetch a resource.
  
 Syntax: let promise = fetch(url.[option])        */
 
+
+//with UI
 const URL = "https://fakestoreapi.com/products/1";
 const dataP = document.querySelector("#desc");
 const btn = document.querySelector("#btn");
@@ -16,9 +18,10 @@ const getSomething = async () => {
     console.log(aData.description);
     dataP.innerText = aData.description;
 }
-
-// fetch('https://fakestoreapi.com/products/1')
-//             .then(res=>res.json())            
-//             .then(json=>console.log(json))
-
 btn.addEventListener("click", getSomething);
+
+
+//example
+fetch('https://fakestoreapi.com/products/1')
+            .then(res=>res.json())            
+            .then(json=>console.log(json))
---
+++ num-manupalation/num.js
@@ -0,0 +1,29 @@
+// Write a program that takes a number as input and checks if it is a prime number.
+//3 -> 3 is a prime number
+
+
+//odd and even
+let num = 50
+
+const rem = num % 2  
+
+if (rem === 0) {
+    console.log(num + " is a even number")
+}
+else {
+    console.log("ODD")
+}
+
+//prime number
+
+function isPrime(num) {
+    // Prime num must be greater than 1
+    if (num <= 1) {   
+        return false;
+    }
+
+    //
+}
+
+// Write a program that takes an array of numbers as input and returns the sum of all the numbers.
+//[1,2,3] -> 6
